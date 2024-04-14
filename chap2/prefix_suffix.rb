064dbb8d2209cfec2f1e94e21b159217b3a47773
tanum-3367 <uong.minh.tan@sun-asterisk.com>
2024-04-14 21:05:41 +0900

    chap2: structure and excution

---
+++ chap2/prefix_suffix.rb
@@ -0,0 +1,21 @@
+class Person
+  def name=(value)
+    @name = value.strip.capitalize
+  end
+
+  def name
+    @name
+  end
+end
+
+person = Person.new
+person.name = " uong minh tan "
+puts person.name
+
+#
+# $files # A global variable
+# @data # An instance variable
+# @@counter # A class variable
+# empty? # A Boolean-valued method or predicate
+# sort! # An in-place alternative to the regular sort method
+# timeout= # A method invoked by assignment
